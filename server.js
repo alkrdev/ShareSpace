1128086db91529e898e488717aa191d569af4982
Alexander <ak@kk-partner.dk>
2020-09-14 13:20:58 +0200

    Test Data Introduction

--- server.js
+++ server.js
@@ -9,20 +9,56 @@ app.use(express.static("build"));
 
 const { Sequelize, Model, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize('database', 'username', 'password', {
+const sequelize = new Sequelize('sharedatabase', 'root', 'Jagex2233', {
     host: 'localhost',
     dialect: "mysql"
 });
 
 class User extends Model {}
-User.init({
-  username: DataTypes.STRING,
-  birthday: DataTypes.DATE
-}, { sequelize, modelName: 'user' });
+class Post extends Model {}
 
-app.get('/allUsers', function(req, res) {
-    const users = User.findAll()    
+(async () => {
+    User.init({
+        username: DataTypes.STRING
+    }, { sequelize, modelName: 'user' });
+    
+    Post.init({
+        title: DataTypes.STRING,
+        text: DataTypes.STRING,
+        type: DataTypes.STRING,
+        comments: DataTypes.STRING,
+        image: DataTypes.STRING,
+        likes: DataTypes.INTEGER
+    }, { sequelize, modelName: 'post' })
+    
+    await sequelize.sync({ force: true });
+    
+    await Post.bulkCreate([{
+        title: "THIS IS A C# SNIPPET", 
+        text: "", 
+        type: "C#", 
+        comments: "Hello, Very good snippet, I actually didn't like this, You are a cool person", 
+        image: "images/1.jpg", 
+        likes: 22
+    }, {
+        title: "THIS IS A ANOTHER C# SNIPPET", 
+        text: "", 
+        type: "C#", 
+        comments: "Hello, Very good snippet, I actually didn't like this, You are a cool person", 
+        image: "images/2.jpg", 
+        likes: 4
+    }])
+    
+    await User.bulkCreate([
+        { username: "Jane Doe" },
+        { username: "John Doe" }
+    ])
+})();
+
+app.get('/allUsers', async function(req, res) {
+    const users = await User.findAll()    
     console.log("All users:", JSON.stringify(users, null, 2));
+    res.send(users);
 })
 
 // start express server on port 5000
